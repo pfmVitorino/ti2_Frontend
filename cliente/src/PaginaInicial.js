31b720e4388b2aa6a9b74a92413120123fc53ab1
Pedro vitorino <PedroVitorino04@hotmail.com>
2019-06-27 18:20:48 +0100

    Os comentários já aparecem na view da páginaInicial Criação de uma função para fechar o popup Criação dos inputs para login e password A autenticação já está a funcionar

--- cliente/src/PaginaInicial.js
+++ cliente/src/PaginaInicial.js
@@ -1,97 +1,168 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import Photograph from './Photograph';
 import Popup from './Popup';
+import './PaginaInicial.css';
 
 
-class PaginaInicial extends Component{
-    constructor(props){
-         super(props);
+class PaginaInicial extends Component {
+    constructor(props) {
+        super(props);
+      // criar estados
+        this.state = {
+            posts: [],
+            ShowPopup: false,
+            ShowImage: {},
+            text: '',
+            Username: '',
+            Password:''
 
-         this.state = {
-             posts: [],
-             ShowPopup: false,
-             ShowImage : {}
-         }
-         // sempre que clik correr o this tem de fazer referencia ao this da pagina principal
-         this.Click = this.Click.bind(this);
-
-    }
-   async componentDidMount(){
-      let response = await axios.get('https://ipt-ti2-iptgram.azurewebsites.net/api/posts');
-      //console.log(response.data[0].caption);
-
-     let postArray = response.data;
-
-     
-     // mudar o estado do objecto
-     this.setState({
-         posts: postArray
-     });
+        }
+        // sempre que clik correr o this tem de fazer referencia ao this da pagina principal
+        this.Click = this.Click.bind(this);
+        this.popupClose = this.popupClose.bind(this);
+        this.searchBy= this.searchBy.bind(this);
+        this.Change = this.Change.bind(this);
+        this.login = this.login.bind(this);
 
     }
+    async componentDidMount() {
+        let response = await axios.get('https://ipt-ti2-iptgram.azurewebsites.net/api/posts');
+        //console.log(response.data[0].caption);
 
-  
-   
- // função que permite clicar na image e aparecer as informações
-   async Click(id){
-        let srt = 'https://ipt-ti2-iptgram.azurewebsites.net/api/posts/'+id
+        let postArray = response.data;
+
+
+        // mudar o estado do objecto
+        this.setState({
+            posts: postArray
+        });
+
+    }
+    // funcao para filtar os dados
+    async searchBy (evt){
+        evt.preventDefault()
+        let response = await axios.get('https://ipt-ti2-iptgram.azurewebsites.net/api/posts?query='+this.state.text);
+
+        let postArray = response.data;
+        // o setState faz com que a pagina inicial seja obrigada a renderizar de novo.(Sempre)
+        this.setState({
+            posts: postArray,
+            text : ''
+        });
+
+    }
+
+
+
+    // função que permite clicar na image e aparecer as informações
+    async Click(id) {
+        let srt = 'https://ipt-ti2-iptgram.azurewebsites.net/api/posts/' + id
         let response = await axios.get(srt);
 
         let obj = {
-            image :  "https://ipt-ti2-iptgram.azurewebsites.net/api/posts/"+id+"/image",
-            user : response.data.user.name,
-            date : response.data.postedAt,
+            image: "https://ipt-ti2-iptgram.azurewebsites.net/api/posts/" + id + "/image",
+            user: response.data.user.name,
+            date: response.data.postedAt,
             subtitle: response.data.caption,
-            likes : response.data.likes
-         
+            likes: response.data.likes,
+            //comments : response.data.comments
+
         };
-   this.setState({
+
+        let commentsResponse = await axios.get('https://ipt-ti2-iptgram.azurewebsites.net/api/posts/' + id + '/comments')
+
+        obj.comments = commentsResponse.data;
+
+        this.setState({
 
 
-  ShowImage : obj,  
-  ShowPopup: true
+            ShowImage: obj,
+            ShowPopup: true
 
-   })
+        });
+    }
+    // função para fechar o popUp
+    popupClose() {
+        this.setState({
+            ShowPopup: false
+        })
 
     }
-    render (){
+
+    // função para a pesquisa
+    // target.name vai buscar o name dos inptus
+   Change(evt){
+       this.setState({
+           [evt.target.name]:evt.target.value 
+       })
+   }
+ // função para o login
+ // login com as credencias fornecidas pelo professor
+   async login(evt){
+       evt.preventDefault();
+       let obj = {
+        "userName": this.state.Username,
+        "password": this.state.Password
+       }
+       let response = await axios.post('https://ipt-ti2-iptgram.azurewebsites.net/api/account/login',obj);
+
+       console.log(response);
+
+   }
+
+
+render(){
+
+    return (
+        <div className="PaginaInicial">
+        <form className="SearchBox" onSubmit={this.searchBy}>
+        <input placeholder="Search..." name = "text" onChange={this.Change} value={this.state.text}/>
         
-              return(
-            <div className="PaginaInicial">
-           {
-               this.state.posts.map(function(p){
-                  // retorna todos os elementos 
-                return([
-                    <h1>{p.caption}</h1>,
-                    <Photograph id = {p.id} Click={this.Click}/>,
-                    <h2>{p.user.name}</h2>,
-                    <h2>{p.postedAt.substring(0,p.postedAt.indexOf("T"))}</h2>,
-                    <h2>{p.likes}</h2>,
-                    <h4>{p.comments}</h4>
-                   
-                ]);
-               }.bind(this))
+        <button type="submit">🔍</button>
 
-              
-               
-            
-            
-           }
-           {
-               // as 2 coisas têm de ser verdade para ele renderizar o popup
-                this.state.ShowPopup && <Popup image = {this.state.ShowImage.image}
-                user = {this.state.ShowImage.user}
-                date =  {this.state.ShowImage.date}
-                subtitle = {this.state.ShowImage.subtitle}
-                likes= {this.state.ShowImage.likes}
+        </form>
+
+        <form className="PaginaInicial-Login" onSubmit={this.login}>
+        <input type="text" name="Username" onChange={this.Change} value={this.state.Username}/>
+        <input type="password" name ="Password" onChange={this.Change} value={this.state.Password}/>
+        <button type= "submit">Login</button>
+        </form>
+            {
+                this.state.posts.map(function (p) {
+                    // retorna todos os elementos 
+                    return ([
+                        <h1>{"Title : "}{p.caption}</h1>,
+                        <Photograph id={p.id} Click={this.Click} />,
+                        <h2>{"Username : "}{p.user.name}</h2>,
+                        <h2>{"Data : "}{p.postedAt.substring(0, p.postedAt.indexOf("T"))}</h2>,
+                        <h2>{"Likes : "}{p.likes}</h2>,
+
+
+                    ]);
+                }.bind(this))
+
+
+
+
+
+            }
+            {
+                // as 2 coisas têm de ser verdade para ele renderizar o popup
+                this.state.ShowPopup && <Popup image={this.state.ShowImage.image}
+                    user={this.state.ShowImage.user}
+                    date={this.state.ShowImage.date}
+                    subtitle={this.state.ShowImage.subtitle}
+                    likes={this.state.ShowImage.likes}
+                    comments={this.state.ShowImage.comments}
+                    popupClose={this.popupClose}
                 />
-           }
-           
-           
-             </div>   
-        );
-    }
+            }
+
+
+        </div>
+    );
+}
 
 
 }
--- cliente/src/Popup.js
+++ cliente/src/Popup.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import './popUp.css'
 
 class Popup extends Component{
+    constructor(props){
+    super(props);
+    this.popupClose = this.popupClose.bind(this);
+   
+    }
+   
+    // chamar a função que fecha o popUp
+
+    popupClose(){
+      this.props.popupClose();
+
+    }
+
+   
+    
     render(){
         return(
             <div className="Popup">
@@ -9,6 +25,24 @@ class Popup extends Component{
               <h2> {this.props.date}</h2>
               <h2> {this.props.subtitle}</h2>
               <h2> {this.props.likes}</h2>
+              
+              {
+                this.props.comments.map(
+                    function(c){
+                        // retorna todos os elementos 
+                        // vai devolver o texto, o user e a data do comentário
+                      return([
+                          <h4>{c.text}</h4>,
+                          <h4>{c.user.name}</h4>,
+                          <h4>{c.postedAt}</h4>
+                         
+                      ]);
+                     }.bind(this))
+                
+              }
+              
+              <button onClick={this.popupClose}>❌</button>
+            
             </div>
 
         );
